4632a9f143448a5c80311ab40b2cb728f7f68b13
Markus Hoesel <markus@linuxhacker.at>
2015-02-21 19:42:21 +0000

    Starting splitting up Activities Controller

--- public/modules/activities/controllers/activities.client.controller.js
+++ public/modules/activities/controllers/activities.client.controller.js
@@ -1,70 +1,89 @@
 'use strict';
 
 // Activities controller
-angular.module('activities').controller('ActivitiesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Activities',
-	function($scope, $stateParams, $location, Authentication, Activities) {
-		$scope.authentication = Authentication;
 
-		// Create new Activity
-		$scope.create = function() {
-			// Create new Activity object
-			var activity = new Activities ({
-				name: this.name,
-				description: this.description,
-				active: this.active
-			});
+var activityApp = angular.module('activities');
 
-			// Redirect after save
-			activity.$save(function(response) {
-				$location.path('activities/' + response._id);
-
-				// Clear form fields
-				$scope.name = '';
-				$scope.description = '';
-				$scope.active = '';
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
-
-		// Remove existing Activity
-		$scope.remove = function(activity) {
-			if ( activity ) { 
-				activity.$remove();
-
-				for (var i in $scope.activities) {
-					if ($scope.activities [i] === activity) {
-						$scope.activities.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.activity.$remove(function() {
-					$location.path('activities');
-				});
-			}
-		};
-
-		// Update existing Activity
-		$scope.update = function() {
-			var activity = $scope.activity;
-
-			activity.$update(function() {
-				$location.path('activities/' + activity._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
-
+activityApp.controller('ActivitiesController', ['$scope', '$stateParams', 'Authentication', 'Activities',
+	function($scope, $stateParams, Authentication, Activities) {
+		
+		this.authentication = Authentication;
+		
 		// Find a list of Activities
-		$scope.find = function() {
-			$scope.activities = Activities.query();
-		};
+		this.activities = Activities.query();
 
-		// Find existing Activity
-		$scope.findOne = function() {
-			$scope.activity = Activities.get({ 
-				activityId: $stateParams.activityId
-			});
-		};
 	}
 ]);
+
+activityApp.controller('ActivitiesCreateController', ['$scope', 'Activities',
+	function($scope, Activities) {
+		
+	}
+]);
+
+activityApp.controller('ActivitiesEditController', ['$scope','Activities',
+	function($scope, Activities) {
+		
+	}
+]);
+		
+
+
+		// // Create new Activity
+		// $scope.create = function() {
+		// 	// Create new Activity object
+		// 	var activity = new Activities ({
+		// 		name: this.name,
+		// 		description: this.description,
+		// 		active: this.active
+		// 	});
+
+		// 	// Redirect after save
+		// 	activity.$save(function(response) {
+		// 		$location.path('activities/' + response._id);
+
+		// 		// Clear form fields
+		// 		$scope.name = '';
+		// 		$scope.description = '';
+		// 		$scope.active = '';
+		// 	}, function(errorResponse) {
+		// 		$scope.error = errorResponse.data.message;
+		// 	});
+		// };
+
+		// // Remove existing Activity
+		// $scope.remove = function(activity) {
+		// 	if ( activity ) { 
+		// 		activity.$remove();
+
+		// 		for (var i in $scope.activities) {
+		// 			if ($scope.activities [i] === activity) {
+		// 				$scope.activities.splice(i, 1);
+		// 			}
+		// 		}
+		// 	} else {
+		// 		$scope.activity.$remove(function() {
+		// 			$location.path('activities');
+		// 		});
+		// 	}
+		// };
+
+		// // Update existing Activity
+		// $scope.update = function() {
+		// 	var activity = $scope.activity;
+
+		// 	activity.$update(function() {
+		// 		$location.path('activities/' + activity._id);
+		// 	}, function(errorResponse) {
+		// 		$scope.error = errorResponse.data.message;
+		// 	});
+		// };
+
+
+
+		// // Find existing Activity
+		// $scope.findOne = function() {
+		// 	$scope.activity = Activities.get({ 
+		// 		activityId: $stateParams.activityId
+		// 	});
+		// };
