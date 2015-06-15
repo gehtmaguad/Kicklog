6c68189b9ec8411e41421c1184ebda8092226527
Markus Hoesel <markus@linuxhacker.at>
2015-06-15 17:54:03 +0000

    -) Fixing core.css: Fallback border-top (hex or RGB) added -) Running grunt build for production use

--- public/dist/application.js
+++ public/dist/application.js
@@ -388,34 +388,6 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
-		$scope.alerts = [
-			{
-				icon: 'glyphicon-user',
-				colour: 'btn-success',
-				total: '124',
-				description: 'TOTAL USERS'
-			},
-			{
-				icon: 'glyphicon-flag',
-				colour: 'btn-info',
-				total: '345',
-				description: 'TOTAL ACTIVITIES'
-			},
-			{
-				icon: 'glyphicon-pencil',
-				colour: 'btn-danger',
-				total: '125334',
-				description: 'TOTAL ENTRIES'
-			},
-			{
-				icon: 'glyphicon-time',
-				colour: 'btn-warning',
-				total: '13465 hours',
-				description: 'TOTAL TIME SPENT'
-			}
-		];
-
-
 	}
 ]);
 'use strict';
