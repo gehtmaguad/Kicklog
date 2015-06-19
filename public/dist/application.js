143701a641c3624ae61b8930c25c30ba3468b8a4
Markus Hoesel <markus@linuxhacker.at>
2015-06-19 18:32:23 +0000

    Styling List Activities and List Entries View

--- public/dist/application.js
+++ public/dist/application.js
@@ -219,7 +219,6 @@ activityApp.controller('ActivitiesCreateController', ['$scope', 'Activities', 'N
 			var activity = new Activities ({
 				name: this.name,
 				description: this.description,
-				active: this.active
 			});
 
 			// Redirect after save
@@ -237,11 +236,49 @@ activityApp.controller('ActivitiesCreateController', ['$scope', 'Activities', 'N
 activityApp.controller('ActivitiesPushController', ['$scope', 'Activities', 'Notify',
 	function($scope, Activities, Notify ) {
 		
+		/* Date Picker */
+  	$scope.open = function($event) {
+    	$event.preventDefault();
+    	$event.stopPropagation();
+
+    	$scope.opened = true;
+  	};
+  	
+		$scope.Date = function(){
+		   return new Date();
+		};
+  
+  	$scope.dateOptions = {
+  	  formatYear: 'yy',
+	    startingDay: 1
+  	};
+
+  	$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  	$scope.format = $scope.formats[0];
+
+  	var tomorrow = new Date();
+  	tomorrow.setDate(tomorrow.getDate() + 1);
+  	var afterTomorrow = new Date();
+  	afterTomorrow.setDate(tomorrow.getDate() + 2);
+  	$scope.events =
+    	[
+      	{
+	        date: tomorrow,
+        	status: 'full'
+      	},
+      	{
+	        date: afterTomorrow,
+        	status: 'partially'
+      	}
+    	];		
+		
 		// Push Entry to Activity
 		this.push = function(pushActivity) {
 			var activity = pushActivity;
 			activity.entries.push({
-				entryText: this.entryText
+				entryText: this.entryText,
+				entryDatePicker: this.entryDatePicker,
+				entryDuration: ( this.entryHours * 60 * 60 ) + ( this.entryMinutes * 60 ) + this.entrySeconds
 			});
 			
 			activity.$update(function() {
