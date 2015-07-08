fab911422662f0c92a2a207535c979e84a0956d0
Markus Hoesel <markus@linuxhacker.at>
2015-07-08 12:52:27 +0000

    Created Custom Filter to display milliseconds human friendly

--- public/modules/activities/controllers/activities.client.controller.js
+++ public/modules/activities/controllers/activities.client.controller.js
@@ -4,6 +4,20 @@
 
 var activityApp = angular.module('activities');
 
+activityApp.filter('millSecondsToTimeString', function() {
+  return function(millseconds) {
+    var seconds = Math.floor(millseconds / 1000);
+    var days = Math.floor(seconds / 86400);
+    var hours = Math.floor((seconds % 86400) / 3600);
+    var minutes = Math.floor(((seconds % 86400) % 3600) / 60);
+    var timeString = '';
+    if(days > 0) timeString += (days > 1) ? (days + " days ") : (days + " day ");
+    if(hours > 0) timeString += (hours > 1) ? (hours + " hours ") : (hours + " hour ");
+    if(minutes >= 0) timeString += (minutes > 1) ? (minutes + " minutes ") : (minutes + " minute ");
+    return timeString;
+};
+});
+
 activityApp.controller('ActivitiesController', ['$scope', '$stateParams', 'Authentication', 'Activities', '$modal', '$log',
 	function($scope, $stateParams, Authentication, Activities, $modal, $log) {
 		
