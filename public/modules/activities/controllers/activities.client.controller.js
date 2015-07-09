d6636f4f507c05a24026fa1b14719cd9f8a71356
Markus Hoesel <markus@linuxhacker.at>
2015-07-09 14:54:14 +0000

    FEAT: Styling Bar Chart

--- public/modules/activities/controllers/activities.client.controller.js
+++ public/modules/activities/controllers/activities.client.controller.js
@@ -183,140 +183,73 @@ activityApp.controller('ActivitiesController', ['$scope', '$stateParams', 'Authe
 	  
 	  $scope.heatMapDataObject = {};
 
+		// Bar Chart
+		$scope.options = {
+		  chart: {
+		      type: 'discreteBarChart',
+		      height: 450,
+		      margin : {
+		          top: 20,
+		          right: 20,
+		          bottom: 60,
+		          left: 55
+		      },
+		      x: function(d){return d.label;},
+		      y: function(d){return d.value;},
+		      showValues: false,
+		      showXAxis: false,
+		      transitionDuration: 0,
+		      xAxis: {
+		          axisLabel: 'X Axis',
+		          axisLabelDistance: 300
+		      },
+		      yAxis: {
+		          axisLabel: 'Y Axis'
+		      },
+		      rotateLabels: 90
+		  }
+		};
+		
+		// Data Object for Bar Chart
+    $scope.data = [
+      {
+        key: "Cumulative Return",
+        values: [ ]
+      }
+  	]	
+
 	  // Find existing Activity
 		$scope.findOne = function() {
 			$scope.activity = Activities.get({ 
 				activityId: $stateParams.activityId
 			});
 			
-			// Heat Map Data Object Callback Function
+			// Activity Data Object Callback Function
 			$scope.activity.$promise.then(function(data) {
 				for(var j in data.entries) {
+					
+					// Heat Map
 					var timestamp = Date.parse(data.entries[j].entryDatePicker)/1000;
 					$scope.heatMapDataObject[timestamp] = 1;
+					
+					// Bar Chart
+					$scope.data[0].values.push({"label": data.entries[j].entryDatePicker,"value":data.entries[j].entryDuration });
 				}				
 			});			
 		};
 
 		// Heat Map Data Summary Object 
-		$scope.heatMapDataObjectSummary = {};
-		Activities.query(function(arr) {
-			var i;
-			for (i = 0; i < arr.length; i++) { 
-				for(var j in arr[i].entries) {
-					var timestamp = Date.parse(arr[i].entries[j].entryDatePicker)/1000;
-					$scope.heatMapDataObjectSummary[timestamp] = 1;
-				}
-			}
-		});
-
-  $scope.options = {
-            chart: {
-                type: 'multiBarHorizontalChart',
-                height: 450,
-                x: function(d){return d.label;},
-                y: function(d){return d.value;},
-                showControls: true,
-                showValues: true,
-                transitionDuration: 500,
-                xAxis: {
-                    showMaxMin: false
-                },
-                yAxis: {
-                    axisLabel: 'Values',
-                    tickFormat: function(d){
-                        return d3.format(',.2f')(d);
-                    }
-                }
-            }
-        };
-        
-       $scope.data = [
-            {
-                "key": "Series1",
-                "color": "#d62728",
-                "values": [
-                    {
-                        "label" : "Group A" ,
-                        "value" : -1.8746444827653
-                    } ,
-                    {
-                        "label" : "Group B" ,
-                        "value" : -8.0961543492239
-                    } ,
-                    {
-                        "label" : "Group C" ,
-                        "value" : -0.57072943117674
-                    } ,
-                    {
-                        "label" : "Group D" ,
-                        "value" : -2.4174010336624
-                    } ,
-                    {
-                        "label" : "Group E" ,
-                        "value" : -0.72009071426284
-                    } ,
-                    {
-                        "label" : "Group F" ,
-                        "value" : -0.77154485523777
-                    } ,
-                    {
-                        "label" : "Group G" ,
-                        "value" : -0.90152097798131
-                    } ,
-                    {
-                        "label" : "Group H" ,
-                        "value" : -0.91445417330854
-                    } ,
-                    {
-                        "label" : "Group I" ,
-                        "value" : -0.055746319141851
-                    }
-                ]
-            },
-            {
-                "key": "Series2",
-                "color": "#1f77b4",
-                "values": [
-                    {
-                        "label" : "Group A" ,
-                        "value" : 25.307646510375
-                    } ,
-                    {
-                        "label" : "Group B" ,
-                        "value" : 16.756779544553
-                    } ,
-                    {
-                        "label" : "Group C" ,
-                        "value" : 18.451534877007
-                    } ,
-                    {
-                        "label" : "Group D" ,
-                        "value" : 8.6142352811805
-                    } ,
-                    {
-                        "label" : "Group E" ,
-                        "value" : 7.8082472075876
-                    } ,
-                    {
-                        "label" : "Group F" ,
-                        "value" : 5.259101026956
-                    } ,
-                    {
-                        "label" : "Group G" ,
-                        "value" : 0.30947953487127
-                    } ,
-                    {
-                        "label" : "Group H" ,
-                        "value" : 0
-                    } ,
-                    {
-                        "label" : "Group I" ,
-                        "value" : 0
-                    }
-                ]
-            }
-        ]        
+		// currently not used
+		// $scope.heatMapDataObjectSummary = {};
+		// Activities.query(function(arr) {
+		// 	var i;
+		// 	for (i = 0; i < arr.length; i++) { 
+		// 		for(var j in arr[i].entries) {
+		// 			var timestamp = Date.parse(arr[i].entries[j].entryDatePicker)/1000;
+		// 			$scope.heatMapDataObjectSummary[timestamp] = 1;
+		// 		}
+		// 	}
+		// });
 		
 	}
 ]);
