d7c6c53957473069e1398e0cc04e645c23d740be
Markus Hoesel <markus@linuxhacker.at>
2015-07-18 11:29:05 +0000

    BUGFIX: Remove multiple directive entry from activities controller

--- public/modules/activities/controllers/activities.client.controller.js
+++ public/modules/activities/controllers/activities.client.controller.js
@@ -246,19 +246,3 @@ angular.module('activities').controller('ActivitiesController', ['$scope', '$sta
 		
 	}
 ]);
-
-activityApp.directive('activityList', ['Activities', 'Notify', function(Activities,Notify) {
-	return {
-		restrict: 'E',
-		transclude: true,
-		templateUrl: 'modules/activities/views/activity-list-template.html',
-		link: function(scope, element, attrs) {
-			
-			// when a new activity is added, update the activity list
-			Notify.getMsg('NewActivity', function(event, data) {
-				// Find a list of Activities
-				scope.activitiesCtrl.activities = Activities.query();
-			});
-		}
-	};
-}]);
