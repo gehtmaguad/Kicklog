c2c513250163eac34027b102d9ff85d80e4fce5f
Markus Hoesel <markus@linuxhacker.at>
2015-07-23 06:59:29 +0000

    Implementing simple random color picker logic

--- public/modules/activities/controllers/activitiesCreate.client.controller.js
+++ public/modules/activities/controllers/activitiesCreate.client.controller.js
@@ -3,12 +3,16 @@
 angular.module('activities').controller('ActivitiesCreateController', ['$scope', 'Activities', 'Notify',
 	function($scope, Activities, Notify ) {
 		
+		var colors = new Array('#81C784','#FFFFFF','#64B5F6','#FFAB91','#A7FFEB',
+			'#CFD8DC','#FFB74D','#004159','#65A8C4','#AACEE2','#9A93EC','#00C590','#00ADCE');
+		
 		// Create new Activity
 		this.create = function() {
 			// Create new Activity object
 			var activity = new Activities ({
 				name: this.name,
 				description: this.description,
+				color: colors[Math.floor(Math.random()*colors.length)]
 			});
 
 			// Redirect after save
