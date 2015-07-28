89e0145724c1adf0a8e895338b684a79e5ec4134
Markus Hoesel <markus@linuxhacker.at>
2015-07-28 15:08:50 +0000

    Creating a more simple landing page

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,30 +5,5 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
-		
-		// Carousel
-		$scope.myInterval = 5000;
-	  var slides = $scope.slides = [];
-	  
-	    slides.push({
-	      image: './modules/core/img/brand/carousel_activities.png',
-	      text: 'create activities'
-	    });	  
-
-	    slides.push({
-	      image: './modules/core/img/brand/carousel_entries.png',
-	      text: 'keep track'
-	    });	  
-	    
-	    slides.push({
-	      image: './modules/core/img/brand/carousel_heatmap.png',
-	      text: 'check status'
-	    });	  
-	    
-	    slides.push({
-	      image: './modules/core/img/brand/carousel_barchart.png',
-	      text: 'show duration'
-	    });	  	    
-
 	}
 ]);
