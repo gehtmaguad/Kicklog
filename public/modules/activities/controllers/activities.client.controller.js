bfed20a48998b32d0981e9e813408c3c56db1d41
Markus Hoesel <markus@linuxhacker.at>
2015-07-29 15:11:18 +0000

    Adding basic statistics

--- public/modules/activities/controllers/activities.client.controller.js
+++ public/modules/activities/controllers/activities.client.controller.js
@@ -218,12 +218,19 @@ activityApp.controller('ActivitiesController', ['$scope', '$stateParams', 'Authe
 				data.entries.sort(function(a,b){
 					return new Date(a.entryDatePicker) - new Date(b.entryDatePicker);
 				});
+				
+				// Summary 
+				var duration_summary = 0;
+				var entry_summary = 0;
 					
 				for(var j in data.entries) {
 					
 					var timestamp = Date.parse(data.entries[j].entryDatePicker)/1000;
 					var localtime = new Date(Date.parse(data.entries[j].entryDatePicker)).toLocaleDateString();
 					var duration = data.entries[j].entryDuration / 60 / 60;
+					
+					duration_summary += data.entries[j].entryDuration;
+					entry_summary += 1;
 					
 					// Heat Map
 
@@ -246,6 +253,11 @@ activityApp.controller('ActivitiesController', ['$scope', '$stateParams', 'Authe
 				for (var label in dataTemp) {
 				    $scope.data[0].values.push({'label': label, 'value': dataTemp[label] });
 				}	
+				
+				// Duration Summary
+				$scope.duration_summary = duration_summary;
+				$scope.entry_summary = entry_summary;
+				$scope.duration_average = duration_summary / entry_summary;
 				
 			});			
 		};
