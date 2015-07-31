d3bcda442cf6b324655bdea710e170479d9ab85f
Markus Hoesel <markus@linuxhacker.at>
2015-07-31 15:59:01 +0000

    Deploying version 1.0.3

--- public/dist/application.js
+++ public/dist/application.js
@@ -525,24 +525,6 @@ angular.module('activities').controller('ActivitiesUpdateController', ['$scope',
 ]);
 'use strict';
 
-angular.module('activities').directive('activityList', ['Activities', 'Notify', function(Activities,Notify) {
-	return {
-		restrict: 'E',
-		transclude: true,
-		templateUrl: 'modules/activities/views/activity-list-template.html',
-		link: function(scope, element, attrs) {
-			
-			// when a new activity is added, update the activity list
-			Notify.getMsg('NewActivity', function(event, data) {
-				// Find a list of Activities
-				scope.activitiesCtrl.activities = Activities.query();
-			});
-		}
-	};
-}
-]);
-'use strict';
-
 angular.module('activities').filter('millSecondsToTimeString', [
 	function() {
 		return function(millseconds) {
